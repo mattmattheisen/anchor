8785fd3fbe3c7444d776b25120b4e0ab4aaa702c
mattmattheisen <mattheisens@frontiernet.net>
2026-08-20 19:10:08 -0500

    Add version information for Anchor's public contract

---
+++ engine/version.py
@@ -0,0 +1,13 @@
+"""
+Version information for Anchor's public decision contract.
+
+The schema version identifies the structure of Anchor's
+serialized output. It is intentionally separate from any
+future application or package version.
+
+Consumers of Anchor output can use this value to determine
+which public output contract they are receiving.
+"""
+
+
+ANCHOR_SCHEMA_VERSION = "1.0"
