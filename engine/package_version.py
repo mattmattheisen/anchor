3a3f614f461aebbbfb4b769afc699c35e3bac6c0
mattmattheisen <mattheisens@frontiernet.net>
2026-08-20 19:38:56 -0500

    Create package_version.py with version details
    Add package version information for Anchor.

---
+++ engine/package_version.py
@@ -0,0 +1,11 @@
+"""
+Package version information for Anchor.
+
+This version identifies the Anchor codebase release.
+
+It is separate from ANCHOR_SCHEMA_VERSION, which identifies
+the structure of Anchor's serialized public output.
+"""
+
+
+ANCHOR_VERSION = "1.0.0"
